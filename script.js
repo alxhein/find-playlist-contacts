00c991db55befc1a331779c4cb5ad2608a88c24d
alxhein <heina@usc.edu>
2020-01-05 23:09:35 -0600

    new login layout

--- script.js
+++ script.js
@@ -101,6 +101,7 @@ class Playlist{
   ];
   
   //declare DOM elements as variables
+  var loginView = document.getElementById('login')
   var authorizeButton = document.getElementById('authorize');
   var body = document.getElementById('body');
   var searchContainer = document.getElementsByClassName('search-container')[0];
@@ -119,7 +120,7 @@ class Playlist{
   
   // check for access token, display buttonList
   if (_token){
-    authorizeButton.style.display = 'none';
+    loginView.style.display = 'none';
     mainContent.style.display = 'block';
     //let playlistIds = ["0Nfd1i1ofRRIXN8Kyk5ms1", "16gXsmg48MdRTqCLNnx9Cu", "1J2vYkrZaRh9mPoRAiVVmU", "7ozIozDp260fjNOZy1yzRG", "3Di88mvYplBtkDBIzGLiiM", "0Zarq4BVkFkZOWkmqsfrjA", "0Wc32Rbhqg6gXSZjxuTYKy", "0NCspsyf0OS4BsPgGhkQXM", "48Mt711afYOOYamgJTMaGF", "4PFT2iFEAmJVAKyS9t0toN", "0OdWlUFdB6Lio5dIdXY81O", "0rkxErbruAmcxwdLL6ZCz8", "6xGLprv9fmlMgeAMpW0x51", "7c8YrzHlapYbk3OpZYi31K", "5CLCReqqp7OTLDMcvB1oLw", "2dmXP0D8ivy0jF3IMuWdsx", "2NdDBIGHUCu977yW5iKWQY", "5Is7WWieaLqwfJ8H7CkcOh", "26zoCb1y8QqOdTXQXZQQFj", "0umA1dm59ONKCi5KIzLoz7", "3aR2n0XpRNlrWose8kx82S", "5JyM6mqPEDLcToVHY5HlY4", "0xrcpsSBNGorqGjSeIgvF6", "1MidghQfbi6j4zS03XTr7T", "6UGRY7ru8yUPoNLa0f4tWv", "2fS5bR8G8wlqt73xndQOGn", "5ffyoghiKFklTNHZIrPJ0q", "3gB2yMGJzoAkh1MVuVBsGA", "2Ps64TVbfmfX6jxWJnlX1j", "0za4lOIjPNqcKUDQqVCGlf", "0B8Wg0ODKFPHCD3lWM4j7P", "48sHE0nZIXRuvre0zhypbD", "782M4g8PfWvszFqPHVnsnR", "5AHH67GYsljwoB1q6UGvWg", "34HOeO7zgTVWsBC61Jea7f", "7zR1nV8comFTN4k97yunVN", "2LVvgP7VJN5xAkmPdl5J4q", "3gWAZPuNWpELIhKNbnpfwk", "54D1ql9kuxp5iDtjV0BNa3", "37D5MPyW7klBxB903ZiGya", "7sZbq8QGyMnhKPcLJvCUFD", "6TJtYZm7f40ed3xLBHN0p2", "7LDpUAjCFTJVKPikrKVSJy", "5Whb5YPTBJeHRoD69BU0ds", "6xUgVgOLgAIqLJ6oTeRXUY", "5RE37ytlCjGvttayQz9Pgj", "1CD11IKUrYwireDIW8oSZi", "2cwdsupS9pMyxb8YMoiOp3", "4rapqxvOH5KKfyhHZDB7lA", "5s3XqdvKhYaepQNQuc6uTe", "5ABMzUESx7K7EyowE5kFCl", "2lfAwrTGw2rbXLJf7elpo9", "6hzruhirL980WrXCGmMxQ3", "7gc654Mavvu8118c6fii6J", "1Jl4zMXcGWW9Qf5rzBQkns", "1pRbBtxQySnKaYh9kA5bwb", "4MGrGUQoW0vgaiDkrwCsjg", "62bReXvmroQzQfEUuTNe3Y", "14q2LelQYB2eqdTLeLbCq7", "30zschNd31QTmpu6qtTOAH", "5Udnh472z0CeexGPSpdIw9", "25WpoeeyHdUYtahseE87dt", "33PyRULhtc4SRrUE1wbbmp", "5ARGJCUqvGavr7y0TFOO18", "0XiPwB7os3uvLvWt2VaJeh", "6uJCrt33VwqvyLf39pcyo5", "73sjn2t4maF758R8bpQFOL", "2GEXzPeksIINQMTivWQ2el", "0C1UDx8LPfsELF6Gu6U5zM", "31oAk1yzLkZajF6gRhqRNV", "6R5Tq68DMutm76RCND9RoT", "25rGszOljOSjPxS5CNVJe0", "4ycIAGqf9BoQMHiNobxGUc", "2sUnJwjAHuUU9Ums6jp6aA", "7nppx7uLgcmkZHg70HNfOk", "3uGGl9F4danv311ezFKerP", "2jcDdyoAdFzRvCdd1jIVWk", "5VmXypMC2ufuwFBwuMkuur", "3SnPVgCtVHMgzcGMsXKuV4", "6Sj2mRpBwLxfHxuQF2lWBG", "6FeRrOVfaeNXlDFfB36jHI", "0VjZFIIqCadnpPBe1quxJo", "2XzbMrcmW3Ka37fVr4V5v9", "333xuRZr7WYZUlsXes8v5p", "3s9XQLSPcI5GdfwB8IsNC7", "549lx4U7hl7dSSRMoGIUga", "0EMTofe0yg487MbIxogliJ", "13BfJYtioEdIE9XRtKZH4h", "3VuM2IVll3qTJOArVERazL", "59aEvelmx0YFKc5WXuric6", "3RKYyyC0t3bOa8m1NpB2c5", "4sgUux9hmykyWYmVoe4W6p", "3JuTvfsK2HEAEkHyuKtza7", "4AycmXrcVLcW0tuDAjGSHo", "2xTej0cHbqpDUG5QHdWVs6", "23qf2c5jdlRAb5WKZANJHA", "1E5vR0mEzAOtisOZfA1Aur", "6mHT3gP1cb5gnQ4O61wC6Y", "3eNKBpYXA6dvARYXGIAIlr"];
     //getEmails(playlistIds, _token);
